be16308dd6bac66b43163136ff3ed73977391c38
Luke Jones <luke@lukeojones.com>
2023-12-18 13:39:48 +0000

    solution(2023,18) Solution to Day 18 - Parts 1 & 2

--- aoc-2023/Day18/main.go
+++ aoc-2023/Day18/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -23,158 +24,95 @@ func main() {
 	}
 
 	lines := strings.Split(input, "\n")
-	instructions := make([]Instruction, len(lines))
+	instructions := parseInstructions(lines)
+	ansP1 := solve(instructions)
 
+	instructions = parseHexInstructions(lines)
+	ansP2 := solve(instructions)
+
+	fmt.Println("Solution to Part 1:", ansP1)
+	fmt.Println("Solution to Part 2:", ansP2)
+}
+
+func parseHexInstructions(lines []string) []Instruction {
+	instructions := make([]Instruction, len(lines))
+	directions := []string{"R", "D", "L", "U"}
 	for l, line := range lines {
 		tokens := strings.Split(line, " ")
-		dir := tokens[0]
-		length, _ := strconv.Atoi(tokens[1])
-		colour := tokens[2]
-		instructions[l] = Instruction{dir, length, colour}
+		hexCode := tokens[2]
+		length := hexToDec(hexCode[2:7])
+		instructions[l] = Instruction{directions[toInt(hexCode[7:8])], int(length), hexCode}
 	}
+	return instructions
+}
 
+func parseInstructions(lines []string) []Instruction {
+	instructions := make([]Instruction, len(lines))
+	for l, line := range lines {
+		tokens := strings.Split(line, " ")
+		instructions[l] = Instruction{tokens[0], toInt(tokens[1]), tokens[2]}
+	}
+	return instructions
+}
+
+func solve(instructions []Instruction) int {
 	curr := image.Point{}
 	var points []image.Point
-	minX, minY := 0, 0
 	for _, instruction := range instructions {
-		for i := 0; i < instruction.length; i++ {
-			p := image.Point{X: curr.X, Y: curr.Y}
-			switch instruction.dir {
-			case "U":
-				p.Y -= 1
-			case "D":
-				p.Y += 1
-			case "L":
-				p.X -= 1
-			case "R":
-				p.X += 1
-			}
-			if p.X < minX {
-				minX = p.X
-			}
-			if p.Y < minY {
-				minY = p.Y
-			}
-			points = append(points, p)
-			curr = p
+		p := image.Point{X: curr.X, Y: curr.Y}
+		switch instruction.dir {
+		case "U":
+			p.Y -= instruction.length
+		case "D":
+			p.Y += instruction.length
+		case "L":
+			p.X -= instruction.length
+		case "R":
+			p.X += instruction.length
 		}
+		points = append(points, p)
+		curr = p
 	}
 
-	// Calculate Offsets
-	offsetX := 0 - minX
-	offsetY := 0 - minY
-
-	maxX, maxY := 0, 0
-	for p, _ := range points {
-		points[p].X += offsetX
-		points[p].Y += offsetY
-		if points[p].X > maxX {
-			maxX = points[p].X
-		}
-		if points[p].Y > maxY {
-			maxY = points[p].Y
-		}
-	}
-
-	// Create Grid
-	grid := make([][]string, maxY+1)
-	for r := 0; r <= maxY; r++ {
-		grid[r] = make([]string, maxX+1)
-		for c := 0; c <= maxX; c++ {
-			if containsPoint(points, image.Point{X: c, Y: r}) {
-				grid[r][c] = "#"
-			} else {
-				grid[r][c] = "."
-			}
-		}
-	}
-
-	// Expand Grid by 1 in each direction
-
-	printGrid(grid)
-	println("=============")
-	grid = expandGrid(grid)
-	printGrid(grid)
-
-	// Part 1 - Flood Fill
-	ansP1 := countLava(grid)
-	//floodFill(grid, 0, 0)
-	//printGrid(grid)
-
-	// Solution here
-	fmt.Println("Solution:", ansP1)
+	return calculatePolygonArea(points)
 }
 
-func expandGrid(grid [][]string) [][]string {
-	rows := len(grid)
-	cols := len(grid[0])
-	newGrid := make([][]string, rows+2)
-
-	// Add top border
-	newGrid[0] = make([]string, cols+2)
-	for c := range newGrid[0] {
-		newGrid[0][c] = "."
-	}
-
-	// Copy the original grid and add left and right borders
-	for i := range grid {
-		newGrid[i+1] = make([]string, cols+2)
-		newGrid[i+1][0] = "."
-		copy(newGrid[i+1][1:], grid[i])
-		newGrid[i+1][cols+1] = "."
-	}
-
-	// Add bottom border
-	newGrid[rows+1] = make([]string, cols+2)
-	for c := range newGrid[rows+1] {
-		newGrid[0][c] = "."
-	}
-
-	return newGrid
-}
-
-func floodFill(grid [][]string, x, y int) {
-	if x < 0 || x >= len(grid) || y < 0 || y >= len(grid[0]) || grid[x][y] != "." {
-		return
-	}
-	grid[x][y] = "o" // Mark the filled cell with 'o'
-	floodFill(grid, x+1, y)
-	floodFill(grid, x-1, y)
-	floodFill(grid, x, y+1)
-	floodFill(grid, x, y-1)
-}
-
-func countLava(grid [][]string) int {
-	floodFill(grid, 0, 0)
-
+func calculatePolygonArea(vertices []image.Point) int {
 	area := 0
-	for _, row := range grid {
-		for _, cell := range row {
-			if cell == "." || cell == "#" {
-				area++
-			}
+	perimeter := 0
+
+	for i := 0; i < len(vertices); i++ {
+		j := (i + 1) % len(vertices)
+		area += vertices[i].X * vertices[j].Y
+		area -= vertices[j].X * vertices[i].Y
+
+		// Calculate perimeter
+		if vertices[i].X == vertices[j].X {
+			perimeter += int(math.Abs(float64(vertices[j].Y - vertices[i].Y)))
+		} else {
+			perimeter += int(math.Abs(float64(vertices[j].X - vertices[i].X)))
 		}
 	}
 
-	return area
+	// Area of the polygon
+	polygonArea := int(math.Abs(float64(area)) / 2.0)
+	return polygonArea + (perimeter / 2) + 1
 }
 
-func printGrid(grid [][]string) {
-	for _, row := range grid {
-		for _, col := range row {
-			fmt.Print(col)
-		}
-		fmt.Println()
+func toInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
 	}
+	return i
 }
 
-func containsPoint(points []image.Point, p image.Point) bool {
-	for _, point := range points {
-		if point == p {
-			return true
-		}
+func hexToDec(s string) int {
+	i, err := strconv.ParseInt(s, 16, 64)
+	if err != nil {
+		panic(err)
 	}
-	return false
+	return int(i)
 }
 
 // Boilerplate: ReadInput reads the input file for the given year and day
